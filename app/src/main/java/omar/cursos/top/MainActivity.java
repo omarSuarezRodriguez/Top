2732e0c6c23c6269bba30b0ab8e6a91092a3e040
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-13 03:48:28 -0500

    Commit

--- app/src/main/java/omar/cursos/top/MainActivity.java
+++ app/src/main/java/omar/cursos/top/MainActivity.java
@@ -1,20 +1,33 @@
 package omar.cursos.top;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerview;
+    @BindView(R.id.containerMain)
+    CoordinatorLayout containerMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
