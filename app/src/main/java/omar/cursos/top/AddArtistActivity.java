611ef837a0c1e597b7d496dadc7bbbcc72a93eca
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-15 04:05:19 -0500

    Commit

---
+++ app/src/main/java/omar/cursos/top/AddArtistActivity.java
@@ -0,0 +1,13 @@
+package omar.cursos.top;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_artist);
+    }
+}
