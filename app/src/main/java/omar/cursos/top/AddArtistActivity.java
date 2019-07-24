dad4792931e0bd6f65ee3daa1a57fb4445391139
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-23 22:49:54 -0500

    Commit

--- app/src/main/java/omar/cursos/top/AddArtistActivity.java
+++ app/src/main/java/omar/cursos/top/AddArtistActivity.java
@@ -1,25 +1,116 @@
 package omar.cursos.top;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.MenuItem;
+import android.widget.DatePicker;
 
-public class AddArtistActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AddArtistActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    @BindView(R.id.imgFoto)
+    AppCompatImageView imgFoto;
+    @BindView(R.id.etNombre)
+    TextInputEditText etNombre;
+    @BindView(R.id.etApellidos)
+    TextInputEditText etApellidos;
+    @BindView(R.id.etFechaNacimiento)
+    TextInputEditText etFechaNacimiento;
+    @BindView(R.id.etEstatura)
+    TextInputEditText etEstatura;
+    @BindView(R.id.etLugarNacimiento)
+    TextInputEditText etLugarNacimiento;
+    @BindView(R.id.etNotas)
+    TextInputEditText etNotas;
+
+
+    private Artista mArtista;
+    private Calendar mCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_artist);
+        ButterKnife.bind(this);
 
 
-        Intent intent = getIntent();
+        configActionBar();
+        configArtista(getIntent());
+        configCalendar();
+
+
+    }
+
+    private void configActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); //Para habilitar la flecha de retroceso ->
+            // -> en la vista
+        }
+    }
+
+    private void configArtista(Intent intent) {
+        mArtista = new Artista();
+        mArtista.setFechaNacimiento(System.currentTimeMillis());
+        mArtista.setOrden(intent.getIntExtra(Artista.ORDEN, 0));
+
+    }
+
+    private void configCalendar() {
+        mCalendar = Calendar.getInstance(Locale.ROOT);
+        etFechaNacimiento.setText(new SimpleDateFormat("dd/MM/yyyy,", Locale.ROOT).format(
+                System.currentTimeMillis()));
+
     }
 
 
     @Override
-    public void onBackPressed() {
-        setResult(RESULT_OK);
-        super.onBackPressed();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.etFechaNacimiento)
+    public void onSetFecha() {
+        DialogSelectorFecha selectorFecha = new DialogSelectorFecha();
+        selectorFecha.setListener(AddArtistActivity.this);
+
+        Bundle args = new Bundle();
+        args.putLong(DialogSelectorFecha.FECHA, mArtista.getFechaNacimiento());
+        selectorFecha.setArguments(args);
+        selectorFecha.show(getSupportFragmentManager(), DialogSelectorFecha.SELECTED_DATE);
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, day);
+
+
+        etFechaNacimiento.setText(new SimpleDateFormat("dd/MM/yyyy", Locale.ROOT).format(
+                mCalendar.getTimeInMillis()));
+        mArtista.setFechaNacimiento(mCalendar.getTimeInMillis());
+
+
+    }
 }
